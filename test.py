d485f3691207897c39bf7d171d47647fd8e4bc0c
wkbscsd <wkbscsd@126.com>
2017-11-25 08:39:16 +0800

    u

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from selenium import webdriver
-
-browser = webdriver.PhantomJS()
-browser.get('http://localhost:8000')
-
-print(browser.title)
-assert 'Django' in browser.title

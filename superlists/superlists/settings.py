d2d94b7dfcc0ae0d37010cf94a81c7148eb28c23
biubiu5174 <qiao5174@qq.com>
2017-11-29 10:34:59 +0800

    homepage add

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'superlists.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR , 'homepage' , 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from homepage import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^page=([0-9]{1,})/$',views.hello),
+
 ]

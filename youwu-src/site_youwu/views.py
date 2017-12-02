075e34d4b0a0393e6daf0b837f92ed7bd8d1abae
wkbscsd <wkbscsd@126.com>
2017-12-02 16:17:42 +0800

    update

--- youwu-src/site_youwu/views.py
+++ youwu-src/site_youwu/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from site_youwu.view_list.view_hello import hello
 from site_youwu.view_list.view_select import select

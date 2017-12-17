b5840ad0838c60432e3b10e039b505009c2cdc30
wkbscsd <wkbscsd@126.com>
2017-12-17 19:32:49 +0800

    update

--- youwu-src/site_youwu/models.py
+++ youwu-src/site_youwu/models.py
@@ -6,11 +6,15 @@ from django.utils import timezone
 class Album(models.Model):
     starId = models.IntegerField(default=0)
     albumId = models.IntegerField(default=0)
+    name = models.CharField(max_length=50, null=True, default="私房照")
     cover = models.CharField(max_length=100, null=True)
     imageListFile = models.CharField(max_length=100, null=True)
     pictureCnt = models.IntegerField(default=0, null=True)
-    # publishDate = models.DateField(null=True)
-    # description = models.TextField(null=True)
+    publishDate = models.DateField(null=True)
+    description = models.TextField(null=True, default="高清套图")
+    company = models.CharField(max_length=50, default="美女写真", null=True)
+    tag = models.CharField(max_length=50, default="", null=True)
+    termId = models.PositiveIntegerField(null=True)
     lastModified = models.DateField(default=timezone.now)
 
 
@@ -35,16 +39,4 @@ class Tags(models.Model):
     tagTypeName = models.CharField(max_length=50)
     tagTypeID = models.IntegerField()
     albumID = models.TextField()
-
-
-
-
-
-
-
-
-
-
-
-
 

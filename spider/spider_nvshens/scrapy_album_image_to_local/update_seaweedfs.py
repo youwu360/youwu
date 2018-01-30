2156882d2101a19127b292a7fa0a2c5514dffa57
wkbscsd <wkbscsd@126.com>
2018-01-30 13:23:49 +0800

    u

---
+++ spider/spider_nvshens/scrapy_album_image_to_local/update_seaweedfs.py
@@ -0,0 +1,52 @@
+import os
+import pycurl
+from io import BytesIO
+import json
+from pyseaweed import WeedFS
+
+
+basePath = os.path.dirname(os.path.realpath(__file__))
+dataPath = os.path.join(basePath, "test_data")
+
+print(dataPath)
+for sub in os.listdir(dataPath):
+    subPath = os.path.join(dataPath, sub)
+    if os.path.isfile(subPath):
+        continue
+
+    print(subPath)
+    for album in os.listdir(subPath):
+        albumPath = os.path.join(subPath, album)
+        if os.path.isfile(albumPath):
+            continue
+
+        albumInJson = album + ".json"
+        albumInJsonPath = os.path.join(subPath, albumInJson)
+
+        processedImg = {}
+        if os.path.exists(albumInJsonPath):
+            fp = open(albumInJsonPath)
+            processedImg = json.load(fp)
+
+        print(albumPath)
+        for img in os.listdir(albumPath):
+            if img in processedImg:
+                continue
+
+            imgFullPath = os.path.join(albumPath, img)
+            print(imgFullPath)
+            w = WeedFS("localhost", 9333)
+            fid = w.upload_file(imgFullPath)
+            img_url = w.get_file_url(fid)
+            #res = w.delete_file(fid)
+
+            processedImg[img] = img_url
+
+            with open(albumInJsonPath, 'w') as fp:
+                json.dump(processedImg, fp)
+            break
+        break
+    break
+
+
+

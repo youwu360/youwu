63e0b948ab133935b3effd9d4810658f934097c9
wkbscsd <wkbscsd@126.com>
2018-02-02 09:21:43 +0800

    u

--- spider/spider_nvshens/scrapy_album_image_to_local/scrapy_album_image_to_local_v2.py
+++ spider/spider_nvshens/scrapy_album_image_to_local/scrapy_album_image_to_local_v2.py
@@ -42,6 +42,7 @@ for starId in joined_json[image_download_helper.starCover]:
     print("in albumImageList, cnt: " + str(cnt))
     if (cnt > 3):
         time.sleep((cnt - 3) / 5.0)
+    break
 
 
 for albumId in joined_json[image_download_helper.albumCover]:
@@ -68,6 +69,7 @@ for albumId in joined_json[image_download_helper.albumCover]:
     if (cnt > 3):
         time.sleep((cnt - 3) / 5.0)
 
+exit(0)
 
 for albumId in joined_json[image_download_helper.albumImageList]:
     starId = joined_json[image_download_helper.albumToStar][albumId]

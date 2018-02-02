115a44c689e355524c8f02de20885ce5f4cec2b8
wkbscsd <wkbscsd@126.com>
2018-02-02 11:42:48 +0800

    u

--- spider/spider_nvshens/scrapy_album_image_to_local/scrapy_album_image_to_local.py
+++ spider/spider_nvshens/scrapy_album_image_to_local/scrapy_album_image_to_local.py
@@ -7,49 +7,90 @@ import time
 import threading
 from scrapy_album_image_to_local_helper import ImageDownloadHelper
 
-basePath = os.path.dirname(os.path.realpath(__file__))
-albumIdFile = os.path.join(basePath, "starAndAlbumId.txt")
-albumImageListFIlePath = "../../../youwu-resource/data/url_info/"
-
-coverUrl = 'https://img.onvshen.com:85/gallery/__sub__/cover/0.jpg'
-
 image_download_helper = ImageDownloadHelper()
 
-with open(albumIdFile, 'r') as fp:
-    lines = fp.readlines()
-    for line in lines:
-        if line is None or line == '':
-            break
-        line = line.strip()
-        if line.startswith('#'):
-            continue
 
-        arr = line.split('/')
-        starId = arr[0]
-        albumId = arr[1]
+basePath = os.path.dirname(os.path.realpath(__file__))
+albumImageListFIlePath = "../../../youwu-resource/data/url_info/"
 
-        subFolder = "data/" + image_download_helper.get_hash_code(starId, albumId)
-        subFolderPath = os.path.join(basePath, subFolder)
-        if not os.path.exists(subFolderPath):
-            os.mkdir(subFolderPath)
 
-        localAlbumDir = os.path.join(subFolderPath, str(starId) + "." + str(albumId))
-        if not os.path.exists(localAlbumDir):
-            os.mkdir(localAlbumDir)
+newly_scrapyed_json_file = os.path.join(basePath, "../../../youwu-src/all_image_urls.json")
+failed_json_file = os.path.join(basePath, "failed.json")
+product_json_file = os.path.join(basePath, "product.json")
 
-        image_url = coverUrl.replace('__sub__', line)
-        image_download_helper.download_album_image(localAlbumDir, image_url, starId, albumId)
 
-        urlFileName = str(starId) + "." + str(albumId)
-        albumImageListFile = os.path.join(basePath, albumImageListFIlePath + urlFileName)
-        print(os.path.abspath(albumImageListFile))
-        with open(albumImageListFile, 'r') as fp:
-            imageUrls = json.load(fp)
-            for image_url in imageUrls:
-                image_download_helper.download_album_image(localAlbumDir, image_url, starId, albumId)
-                cnt = threading.active_count()
-                print(cnt)
-                if (cnt > 3):
-                    time.sleep((cnt - 3) / 5.0)
+newly_scrapyed_json = image_download_helper.load_file_to_json(newly_scrapyed_json_file)
+last_failed_json = image_download_helper.load_file_to_json(failed_json_file)
+product_json = image_download_helper.load_file_to_json(product_json_file)
+
+joined_json = image_download_helper.join_result(newly_scrapyed_json, last_failed_json,
+                                                image_download_helper.add)
+joined_json = image_download_helper.join_result(joined_json, product_json,
+                                                image_download_helper.minus)
+
+failed_json = image_download_helper.default_dict()
+
+
+for starId in joined_json[image_download_helper.starCover]:
+    url = joined_json[image_download_helper.starCover][starId]
+    subFolder = "cover/" + image_download_helper.get_hash_code(starId, None)
+    subFolderPath = os.path.join(basePath, subFolder)
+    if not os.path.exists(subFolderPath):
+        os.mkdir(subFolderPath)
+    image_download_helper.download_star_cover(subFolderPath, url, starId)
+    cnt = threading.active_count()
+    print("in albumImageList, cnt: " + str(cnt))
+    if (cnt > 3):
+        time.sleep((cnt - 3) / 5.0)
+
+
+for albumId in joined_json[image_download_helper.albumCover]:
+    url = joined_json[image_download_helper.albumCover][albumId]
+    if albumId in joined_json[image_download_helper.albumToStar]:
+        starId = joined_json[image_download_helper.albumToStar][albumId]
+    else:
+        print('----------')
+        print("albumId is null")
+        print(url)
+        continue
+
+    subFolder = "data/" + image_download_helper.get_hash_code(starId, albumId)
+    subFolderPath = os.path.join(basePath, subFolder)
+    if not os.path.exists(subFolderPath):
+        os.mkdir(subFolderPath)
+
+    localAlbumDir = os.path.join(subFolderPath, str(starId) + "." + str(albumId))
+    if not os.path.exists(localAlbumDir):
+        os.mkdir(localAlbumDir)
+
+    image_download_helper.download_album_cover(localAlbumDir, url, starId, albumId)
+    cnt = threading.active_count()
+    print("in albumImageList, cnt: " + str(cnt))
+    if (cnt > 3):
+        time.sleep((cnt - 3) / 5.0)
+
+
+for albumId in joined_json[image_download_helper.albumImageList]:
+    starId = joined_json[image_download_helper.albumToStar][albumId]
+    url = joined_json[image_download_helper.albumCover][albumId]
+    subFolder = "data/" + image_download_helper.get_hash_code(starId, albumId)
+    subFolderPath = os.path.join(basePath, subFolder)
+    if not os.path.exists(subFolderPath):
+        os.mkdir(subFolderPath)
+
+    localAlbumDir = os.path.join(subFolderPath, str(starId) + "." + str(albumId))
+    if not os.path.exists(localAlbumDir):
+        os.mkdir(localAlbumDir)
+
+    albumDict = joined_json[image_download_helper.albumImageList][albumId]
+    for img in albumDict:
+        imgUrl = albumDict[img]
+        image_download_helper.download_album_image(localAlbumDir, imgUrl, starId, albumId)
+        cnt = threading.active_count()
+        print("in albumImageList, cnt: " + str(cnt))
+        if (cnt > 3):
+            time.sleep((cnt - 3) / 5.0)
+
+time.sleep(10)
 
 

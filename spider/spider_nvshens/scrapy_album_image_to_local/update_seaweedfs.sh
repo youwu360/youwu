c43aa919345b01155352aaaa024d82c31b7d230b
Your Name <you@example.com>
2018-02-02 02:58:49 +0000

    u

--- spider/spider_nvshens/scrapy_album_image_to_local/update_seaweedfs.sh
+++ spider/spider_nvshens/scrapy_album_image_to_local/update_seaweedfs.sh
@@ -2,7 +2,16 @@
 t=`date +%s`
 log=logs/log.${t}
 
+
+
 date > ${log}
+a=`ps aux | grep update_seaweedf | grep -v grep |wc -l`
+if [ $a -gt 0 ]
+then
+    echo "update_seaweedf is executing , exit now !"
+    exit
+fi
+
 /usr/bin/python3 update_seaweedfs.py >> ${log}
 date >> ${log}
 

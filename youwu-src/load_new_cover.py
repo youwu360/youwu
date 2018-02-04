47225783e2dd87cb002a935702b2541f76e0c4c6
wkbscsd <wkbscsd@126.com>
2018-02-04 21:04:22 +0800

    u

--- youwu-src/load_new_cover.py
+++ youwu-src/load_new_cover.py
@@ -12,7 +12,7 @@ from load_nvshens_helper import insert_star, insert_album, insert_tags
 import datetime
 
 
-allUrlsForProductInJsonFile = '/tmp/allUrlsForProduct.json'
+allUrlsForProductInJsonFile = 'allUrlsForProduct.json'
 allUrlsForProductInJson = None
 if os.path.exists(allUrlsForProductInJsonFile):
     allUrlsForProductInJson = json.load(open(allUrlsForProductInJsonFile))
@@ -54,7 +54,53 @@ class LoadNewCover:
                 print("try update fail in updated album")
                 print(e)
 
+class CleanNullAlbum:
+
+    toSave = {}
+    toDelete = {}
+    def __init__(self):
+        albumIdToList = {}
+        if albumImageList in allUrlsForProductInJson:
+            albumIdToList = allUrlsForProductInJson[albumImageList]
+
+        for albumId in albumIdToList:
+            imgList = albumIdToList[albumId]
+            if len(imgList) > 0:
+                self.toSave[albumId] = True
+            else:
+                self.toDelete[albumId] = True
+
+    def delete_album(self):
+
+        albums = Album.objects.all()
+
+        cnt = 0
+        for album in albums:
+            albumId = str(album.albumId)
+            if albumId in self.toSave:
+                continue
+
+            cnt += 1
+            print("album.delete()")
+            print(album.albumId)
+            print("cnt: " + str(cnt))
+
+        print(len(self.toSave))
+        print(len(self.toDelete))
+
+        # for albumId in allUrlsForProductInJson[albumCover]:
+        #     url = allUrlsForProductInJson[albumCover][albumId]
+        #     try:
+        #         if Album.objects.filter(albumId=albumId).exists():
+        #             v = Album.objects.get(albumId=albumId)
+        #             v.cover = json.dumps([url])
+        #             v.save()
+        #             print("updated albumId : " + str(albumId))
+        #     except Exception as e:
+        #         print("try update fail in updated album")
+        #         print(e)
+
+
 if __name__ == '__main__':
-    loader = LoadNewCover()
-    loader.loadStarCover()
-    loader.loadAlbumCover()
+    cleaner = CleanNullAlbum()
+    cleaner.delete_album()

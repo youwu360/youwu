241edde943e543dd276f324d7e9e6aac9d1b86c5
wkbscsd <wkbscsd@126.com>
2018-02-04 08:22:29 +0800

    u

---
+++ youwu-src/load_new_cover.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basics.settings")
+import django
+import re
+import time
+django.setup()
+import json
+import random
+from site_youwu.models import Star, Album, Tags
+from load_nvshens_helper import insert_star, insert_album, insert_tags
+import datetime
+
+
+allUrlsForProductInJsonFile = '/tmp/allUrlsForProduct.json'
+allUrlsForProductInJson = None
+if os.path.exists(allUrlsForProductInJsonFile):
+    allUrlsForProductInJson = json.load(open(allUrlsForProductInJsonFile))
+
+starCover = 'starCover'
+albumCover = 'albumCover'
+albumToStar = 'albumToStar'
+albumImageList = 'albumImageList'
+
+
+
+class LoadNewCover:
+
+    def loadStarCover(self):
+        for starId in allUrlsForProductInJson[starCover]:
+            url = allUrlsForProductInJson[starCover][starId]
+            try:
+                if Album.objects.filter(starId=starId).exists():
+                    v = Star.objects.get(starId=starId)
+                    v.cover = json.dumps([url])
+                    v.save()
+                    print("updated starId : " + str(starId))
+            except Exception as e:
+                print("try update fail in updated star")
+                print(e)
+            break
+
+
+    def loadAlbumCover(self):
+        for albumId in allUrlsForProductInJson[albumCover]:
+            url = allUrlsForProductInJson[albumCover][albumId]
+            try:
+                if Album.objects.filter(albumId=albumId).exists():
+                    v = Album.objects.get(albumId=albumId)
+                    v.cover = json.dumps([url])
+                    v.save()
+                    print("updated albumId : " + str(albumId))
+            except Exception as e:
+                print("try update fail in updated album")
+                print(e)
+
+if __name__ == '__main__':
+    loader = LoadNewCover()
+    loader.loadStarCover()
+    loader.loadAlbumCover()
